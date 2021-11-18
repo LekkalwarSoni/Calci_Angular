8456d5adaee1f85d15178456cbd3b7514a22f436
Soni Sanjay Lekkalwar <slekkalwar@innominds.com>
2021-11-18 13:31:46 +0530

    handling and state done

--- mycalci/src/app/global_components/header/header.component.ts
+++ mycalci/src/app/global_components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  history_count:Number = 0;
   constructor() { }
 
   ngOnInit(): void {
